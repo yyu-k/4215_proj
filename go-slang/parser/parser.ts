51126cce772b3bcb38e995195ffbace0cf469df6
Benjamin Tan <benjamin@dev.ofcr.se>
2024-03-21 01:12:38 +0800

    Replace `pegjs` with `peggy`
    `pegjs` was not being maintained and was seriously outdated.

--- go-slang/parser/parser.ts
+++ go-slang/parser/parser.ts
@@ -1,7 +1,7 @@
-import * as peg from "pegjs";
+import * as peg from "peggy";
 import * as fs from 'node:fs';
 import * as path from 'path';
 
-//grammer modified from https://github.com/hachi8833/golang_spec_peg_pigen
-const grammar = fs.readFileSync(path.resolve(__dirname, "./grammar.peg")).toString();
+// Grammar modified from https://github.com/hachi8833/golang_spec_peg_pigen
+const grammar = fs.readFileSync(path.resolve(__dirname, "./grammar.peg"), "utf-8");
 export const parser = peg.generate(grammar);
